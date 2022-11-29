f8304a043e920c6287d6823f24c7b395992351e5
Awe Ayomidipupo <pupoawe@gmail.com>
2022-11-29 04:41:49 +0100

    fix: pad output with 0s

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,6 +4,6 @@ for i in range(0, 100):
     #  than the tenth digit i.e in 45, 5 is larger than 4
     if (i % 10) - ((i/10) % 10) > 0:
         if i != 89:
-            print("{}, ".format(i), end="")
+            print("{:0>2}, ".format(i), end="")
         else:
             print("{}".format(i))
