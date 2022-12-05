4b159748bdf9cb194b315d0c7d039457c351427a
Awe Ayomidipupo <pupoawe@gmail.com>
2022-12-05 14:57:48 +0100

    print new line for empty row

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,8 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
+        if len(row) == 0:
+            print("")
         for idx, item in enumerate(row):
             if idx != len(row) - 1:
                 print("{:d}".format(item), end=" ")
