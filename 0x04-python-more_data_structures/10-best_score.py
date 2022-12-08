59631d0e7a643baad8377ff1967fb51e415deb97
Awe Ayomidipupo <pupoawe@gmail.com>
2022-12-08 01:53:25 +0100

    fix: return correct output in task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary == None or len(a_dictionary) == 0:
         return None
 
-    max = 0
+    max = None
 
     for key in a_dictionary.keys():
-        if a_dictionary[key] > max:
-            max = a_dictionary[key]
+        if max == None:
+            max = key
+        elif a_dictionary[key] > a_dictionary[max]:
+            max = key
 
     return max
