6231cd15f5833fa053bb6f6a070981077bf5d7f0
Awe Ayomidipupo <pupoawe@gmail.com>
2022-12-19 14:31:28 +0100

    fix: add except block

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,8 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print("{}".format(my_list[i]), end="")
             count += 1
+    except:
+        pass
     finally:
         print("")
         return count
