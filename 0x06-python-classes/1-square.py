d7cf6e3df7a5ea2c1f236c9cfe39143003af4d31
Awe Ayomidipupo <pupoawe@gmail.com>
2022-12-20 23:05:46 +0100

    feat: add private attribute __size to Square class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""0-Square Module
+
+This module contains an empty class definition for a
+square
+
+"""
+
+
+class Square:
+    """This is an empty class that doesn't nothing
+
+    """
+
+    def __init__(self, size) -> None:
+        """Constructor for Square class
+
+        Args:
+            size (int): size of square
+        """
+        self.__size = size  # size of square
