6e3e41ef5613a2c9d7410c7a7c02a6cb20b98f95
Awe Ayomidipupo <pupoawe@gmail.com>
2022-12-20 23:25:24 +0100

    feat: add instance "area" instance method to Square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""2-Square Module
+
+This module contains a class definition for a
+square
+
+"""
+
+
+class Square:
+    """This class represents a square of definite size
+
+    """
+
+    def __init__(self, size=0):
+        """Constructor for Square class
+
+        Args:
+            size (int): size of square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size  # size of square
+
+    def area(self):
+        """Returns current area of the square
+        """
+        return self.__size ** 2
