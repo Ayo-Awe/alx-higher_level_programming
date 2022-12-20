2fa83bdaa74a401dc67ad3371bb202348021a420
Awe Ayomidipupo <pupoawe@gmail.com>
2022-12-20 23:40:32 +0100

    feat: add setter and getter for size property

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+"""4-Square Module
+
+This module contains a class definition for a
+square
+
+"""
+
+
+class Square:
+    """This class represents a square of definite size
+
+    """
+
+    def __init__(self, size=0):
+        """Constructor for Square class
+
+        Args:
+            size (int): size of square
+        """
+        self.size = size  # size of square
+
+    def area(self):
+        """Returns current area of the square
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Getter for private size attribute
+        """
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter for private size attribute
+
+        This helps centralize validation
+
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
