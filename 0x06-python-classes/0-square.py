a476f6f7e596333e10d42f32d9e7e6db5fd47d7e
Awe Ayomidipupo <pupoawe@gmail.com>
2022-12-20 22:54:27 +0100

    feat: create empty class Square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""0-Square Module
+
+This module contains an empty class definition for a
+square
+
+"""
+
+
+class Square:
+    """This is an empty class that doesn't nothing
+
+    """
+    pass
