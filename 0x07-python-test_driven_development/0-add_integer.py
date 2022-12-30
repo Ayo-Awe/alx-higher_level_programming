f6140e69dd73da6f180aa02d6557406740554265
Awe Ayomidipupo <pupoawe@gmail.com>
2022-12-30 11:13:12 +0100

    feat: solve task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Add Integer Module"""
+
+
+def add_integer(a, b=98):
+    """add_integer function
+
+    The add_integer function adds two integers together
+    and returns the result
+
+    Args:
+                a (int): Integer or float
+                b (int): Integer or float
+    """
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
