8d257a778094715830c44bb2c655245a86157f02
Awe Ayomidipupo <pupoawe@gmail.com>
2022-12-31 14:24:59 +0100

    feat: solve task 4

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Text Indentation Module
+
+    This Module contains code for the text indentation task at ALX
+"""
+
+
+def text_indentation(text):
+    """text_indentation function
+
+        The text_indentation function prints a 2 new line characters
+        when it encounters any of these characters ., ? and :
+
+        Args:
+                text (string): String containing text
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    trimmed_text = text.strip()
+    new_line = False
+    delimeters = ['.', ':', '?']
+
+    for c in trimmed_text:
+
+        if new_line and c == ' ':
+            # skip spaces at beginning of line
+            continue
+        else:
+            new_line = False
+            print(c, end="")
+
+            # print 2 new lines if is delimeter
+        if c in delimeters:
+            print("\n")
+            new_line = True
