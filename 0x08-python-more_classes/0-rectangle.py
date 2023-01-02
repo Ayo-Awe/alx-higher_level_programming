1a53755e2a1c9b82d1302e4f21cf509218d12001
Awe Ayomidipupo <pupoawe@gmail.com>
2023-01-02 12:28:22 +0100

    feat: solve task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Rectangle Class Module
+
+This module contains a class definition for a rectangle
+"""
+
+
+class Rectangle:
+    """ Rectangle Class
+
+    This is definition for an empty rectangle class
+    """
