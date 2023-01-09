7efda384a23de07d086278a68535e7f524b0fb6b
Awe Ayomidipupo <pupoawe@gmail.com>
2023-01-09 16:38:15 +0100

    feat(0x0A): solve task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""base geometry Module
+This module contains code for task 6 of the alx
+python inheritance project
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry Class
+        Instance Methods:
+            area(public)
+    """
+
+    def area(self):
+        """area Function
+        The area function simply raises an exception
+        """
+        raise Exception("area() is not implemented")
+    pass
