2aba23059462559b76224c938bf07376598a3ee3
Awe Ayomidipupo <pupoawe@gmail.com>
2023-01-09 17:34:31 +0100

    feat(0x0A): solve task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""My List Module
+This module contains code for task 1 of the alx
+python inheritance project
+"""
+
+
+class MyList(list):
+    """MyList Class
+        Inherits from list
+    """
+
+    def print_sorted(self):
+        """print_sorted Function
+        Prints a python list but sorted in ascending order
+        """
+        # Create a copy of the list
+        copy = self.copy()
+
+        # Sort the copy of the list
+        copy.sort()
+
+        print(copy)
