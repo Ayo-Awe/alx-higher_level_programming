b20e7c8824afcd3b7bc5b517d574facf4bbbf984
Awe Ayomidipupo <pupoawe@gmail.com>
2023-01-09 16:55:52 +0100

    feat(0x0A): solve task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""base geometry Module
+This module contains code for task 7 of the alx
+python inheritance project
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry Class
+        Instance Methods:
+            area(public)
+    """
+
+    def area(self):
+        """area Function
+        The area function simply raises an exception
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """integer_validation Function
+        This function validates value
+            Args:
+            name(string): is always a string
+            value(int): integer greater than zero
+        """
+
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+    pass
