b6577cb9d103324d88f3e8218e7466435191fefd
Awe Ayomidipupo <pupoawe@gmail.com>
2023-01-09 16:33:52 +0100

    feat(0x0A): solve task 5

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""base geometry Module
+This module contains code for task 5 of the alx
+python inheritance project
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry Class
+    """
+    pass
