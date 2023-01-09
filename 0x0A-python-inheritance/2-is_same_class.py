2c1d2a70e7cb832d53f6c35bd1b20a5f7b46ee1f
Awe Ayomidipupo <pupoawe@gmail.com>
2023-01-09 16:20:11 +0100

    feat(0x0A): solve task 3

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -14,4 +14,4 @@ def is_same_class(obj, a_class):
             obj(object): object
             a_class: parent class
     """
-    return isinstance(obj, a_class)
+    return type(obj) is a_class
--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""is_kind_of_class
+This module contains code for task 3 of the alx
+python inheritance project
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """is_kind_of_class Function
+    This function returns true if an object (obj) is
+    derived from another parent class or is of the
+    same class
+
+    Args:
+            obj(object): object
+            a_class: parent class
+    """
+    return isinstance(obj, a_class)
