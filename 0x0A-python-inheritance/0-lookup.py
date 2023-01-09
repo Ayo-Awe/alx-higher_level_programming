560d8c8be29daf8499146dae2858bac3a0933700
Awe Ayomidipupo <pupoawe@gmail.com>
2023-01-09 15:11:47 +0100

    doc(0x0A): add documentation to the lookup script

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
 
+""" This module contains code for the lookup task
+in alx higher level programming.
+"""
+
+
 def lookup(obj):
+    """Lookup Function
+    The lookup function returns a list containing all the available attributes
+    and methods available for an object
+
+                Args:
+                        obj(object): any valid python object
+    """
     attributes = dir(obj)
     return list(attributes)
