ed559a56399e9a970c9003bb11f38a5d6bfdf1fd
Awe Ayomidipupo <pupoawe@gmail.com>
2023-02-15 19:04:05 +0100

    feat(0x0E): solve task 15

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,8 @@
+--  lists all Comedy shows in the database
+-- hbtn_0d_tvshows.
+SELECT tv_shows.title
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres.genre_id = (SELECT id FROM tv_genres WHERE name="Comedy")
+ORDER BY tv_shows.title ASC;
