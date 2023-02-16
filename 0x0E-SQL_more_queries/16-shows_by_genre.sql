4bed2378c7d89afc9ed6049683401fc1c3348771
Awe Ayomidipupo <pupoawe@gmail.com>
2023-02-16 13:37:00 +0100

    fix(0x0E): use appropriate joins

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -2,8 +2,8 @@
 -- from the database hbtn_0d_tvshows.
 SELECT tv_shows.title, tv_genres.name
 FROM tv_show_genres
-INNER JOIN tv_shows
-ON tv_show_genres.show_id=tv_shows.id
-INNER JOIN tv_genres
-ON tv_show_genres.genre_id=tv_genres.id
-ORDER BY tv_shows.title, tv_genres.name ASC;
+RIGHT JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title ASC;
