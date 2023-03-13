986ee404e3867c91dd6f4a202e9c6d83a2a8a53f
Awe Ayomidipupo <pupoawe@gmail.com>
2023-03-13 20:53:39 +0100

    feat(0x12): solve task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const args = process.argv;
+
+const numbers = args.slice(2);
+
+if (numbers.length === 0) {
+  console.log(0);
+} else if (numbers.length === 1) {
+  console.log(1);
+} else {
+  numbers.sort((a, b) => b - a);
+  console.log(Number(numbers[1]));
+}
