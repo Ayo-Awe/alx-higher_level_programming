5a437c819874282711459060baa69511760d76eb
Awe Ayomidipupo <pupoawe@gmail.com>
2023-03-14 08:18:54 +0100

    fix: resolve code requirement error

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,10 +3,8 @@ const args = process.argv;
 
 const numbers = args.slice(2);
 
-if (numbers.length === 0) {
+if (numbers.length <= 1) {
   console.log(0);
-} else if (numbers.length === 1) {
-  console.log(1);
 } else {
   numbers.sort((a, b) => b - a);
   console.log(Number(numbers[1]));
