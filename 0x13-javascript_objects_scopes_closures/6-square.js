22138539c48739f71fa661b27d37912d6939c91c
Awe Ayomidipupo <pupoawe@gmail.com>
2023-03-14 23:24:17 +0100

    feat(0x13): solve task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const OldSquare = require('./5-square');
+
+class Square extends OldSquare {
+  charPrint (c = 'X') {
+    const row = c.repeat(this.width);
+    for (let i = 0; i < this.height; i++) console.log(row);
+  }
+}
+
+module.exports = Square;
