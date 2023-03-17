5a26db6a652eb1bb2bc54da9d86c74b43bdb64e8
Awe Ayomidipupo <pupoawe@gmail.com>
2023-03-17 10:57:56 +0100

    feat(0x0F): solve task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""MySQLClient task to select state"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    connect_options = {
+        "user":  sys.argv[1],
+        "password": sys.argv[2],
+        "database": sys.argv[3],
+        "host": "localhost",
+        "port": 3306
+    }
+    conn = MySQLdb.connect(**connect_options)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    results = cursor.fetchall()
+
+    for row in results:
+        print(row)
+    cursor.close()
+    conn.close()
