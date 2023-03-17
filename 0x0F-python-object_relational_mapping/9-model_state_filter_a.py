89d568d6198c9e8c68c891d4d43c419d3bd98d28
Awe Ayomidipupo <pupoawe@gmail.com>
2023-03-18 00:57:37 +0100

    feat(0x0F): add script to list states with 'a'

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This module contains code to fetch states that
+contain the letter 'a' from a database using an ORM.
+This script takes the following positional arguments
+Args:
+    1: <database user>
+    2: <user password>
+    3: <database>
+"""
+from model_state import State, Base
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    user, pwd, db = sys.argv[1:]
+    engine = create_engine(f"mysql+mysqldb://{user}:{pwd}@localhost:3306/{db}")
+    engine.connect()
+    Base.metadata.create_all(engine)
+    session = Session(bind=engine)
+
+    states = session.query(State).filter(State.name.like("%a%"))
+
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+    session.close()
