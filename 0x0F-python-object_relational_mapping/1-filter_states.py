1ddb85d9f5cc5819146a906a0425385d9a3aa67d
Awe Ayomidipupo <pupoawe@gmail.com>
2023-03-18 13:09:40 +0100

    feat(0x0F): use BINARY for case sensitive search

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,20 +3,34 @@
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    connect_options = {
-        "user":  sys.argv[1],
-        "password": sys.argv[2],
-        "database": sys.argv[3],
-        "host": "localhost",
-        "port": 3306
-    }
-    conn = MySQLdb.connect(**connect_options)
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-    results = cursor.fetchall()
 
-    for row in results:
-        print(row)
-    cursor.close()
-    conn.close()
+def filter_states():
+    """This function fetchs a list of
+    states starting with the letter 'N'
+    """
+    try:
+        user, pwd, db = sys.argv[1:]
+        connect_options = {
+            "user":  user,
+            "password": pwd,
+            "database": db,
+            "host": "localhost",
+            "port": 3306
+        }
+        conn = MySQLdb.connect(**connect_options)
+        cursor = conn.cursor()
+        query = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
+        cursor.execute(query)
+        results = cursor.fetchall()
+
+        for row in results:
+            print(row)
+
+        cursor.close()
+        conn.close()
+    except Exception as e:
+        return e
+
+
+if __name__ == "__main__":
+    filter_states()
