9bea0c65df8b32fd3b981410c4b087930e5aa4e0
Awe Ayomidipupo <pupoawe@gmail.com>
2023-03-18 01:06:11 +0100

    feat(0x0F): add script to search for state by name

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This module contains code to fetch the state whose name
+matches the query string  using an ORM.
+This script takes the following positional arguments
+Args:
+    1: <database user>
+    2: <user password>
+    3: <database>
+    4: <search string>
+"""
+from model_state import State, Base
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    user, pwd, db, search_string = sys.argv[1:]
+    engine = create_engine(f"mysql+mysqldb://{user}:{pwd}@localhost:3306/{db}")
+    engine.connect()
+    Base.metadata.create_all(engine)
+    session = Session(bind=engine)
+
+    match = session.query(State).filter(
+        State.name == search_string).one_or_none()
+
+    if match:
+        print(match.id)
+    else:
+        print("Not Found")
+
+    session.close()
