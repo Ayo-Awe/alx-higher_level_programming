f9e0590acdd061408a9c9291210872cfbb84a12b
Awe Ayomidipupo <pupoawe@gmail.com>
2023-03-18 01:38:00 +0100

    feat(0x0F): add script to delete row using ORM

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This module contains code to delete all states
+containing the letter 'a' in their name
+This script takes the following positional arguments
+Args:
+    1: <database user>
+    2: <user password>
+    3: <database>
+"""
+from model_state import State, Base
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    user, pwd, db = sys.argv[1:]
+    engine = create_engine(f"mysql+mysqldb://{user}:{pwd}@localhost:3306/{db}")
+    engine.connect()
+    Base.metadata.create_all(engine)
+    session = Session(bind=engine)
+
+    session.query(State).filter(State.name.like(
+        "%a%")).delete(synchronize_session="auto")
+    session.commit()
+
+    session.close()
