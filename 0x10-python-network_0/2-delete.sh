49bd0309e17802f2d3f86a8b6d159291f5b64ce3
Awe Ayomidipupo <pupoawe@gmail.com>
2023-03-30 22:07:31 +0100

    further script bug fixes

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Script to make delete request
 curl -s -X "DELETE" "$1"
-
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script to display allowed http methods
-curl --head -s "$1" | grep "Allow" | cut -d " " -f 2,3,4
+curl --head -s "$1" | grep "Allow" | cut -d " " -f 2-
