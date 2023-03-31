84a828effe01af28c9da67307424ac5432fbd1bf
Awe Ayomidipupo <pupoawe@gmail.com>
2023-03-31 23:35:48 +0100

    fix: resolve pycodestyle errors

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,15 +8,14 @@ import sys
 def get_commits():
     """Query GitHub API for commits
     """
-
     try:
-        repo, owner = sys.argv[1:]
-        url = f"https://api.github.com/repos/{owner}/{repo}/commits?per_page=10"
+        repo, ownr = sys.argv[1:]
+        url = f"https://api.github.com/repos/{ownr}/{repo}/commits?per_page=10"
         res = requests.get(url)
         body = res.json()
         for res in body:
             print(f"{res['sha']}: {res['commit']['author']['name']}")
-    except:
+    except Exception:
         pass
 
 
