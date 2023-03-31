a76b8eaf8707a3efc9f2868fb51fc3c5bc3dd5c3
Awe Ayomidipupo <pupoawe@gmail.com>
2023-03-31 11:01:55 +0100

    fix: output body from redirection

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script to display body of 200 responses
-curl -Lsi "$1" | head -n 1 | grep "200" > /dev/null && curl -s "$1"
+curl -Lsi "$1" | head -n 1 | grep "200" > /dev/null && curl -Ls "$1"
