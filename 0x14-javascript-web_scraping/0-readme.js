5a3468ac2fb928381c832bb7d240d131021af998
Awe Ayomidipupo <pupoawe@gmail.com>
2023-04-25 06:24:07 +0100

    fix: resolve semistandart linting errors

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const fs = require("fs");
+const fs = require('fs');
 
 const filePath = process.argv[2];
-fs.readFile(filePath, { encoding: "utf-8" }, (err, data) => {
+fs.readFile(filePath, { encoding: 'utf-8' }, (err, data) => {
   if (err) return console.log(err);
   console.log(data);
 });
