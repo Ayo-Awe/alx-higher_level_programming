e45f0c66850159e6da95427b7c5655f9d74dede4
Awe Ayomidipupo <pupoawe@gmail.com>
2023-04-25 07:10:56 +0100

    fix: remove default console.log

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -8,7 +8,7 @@ const filePath = process.argv[3];
 request.get({ url }, (error, response, body) => {
   if (!error && response.statusCode === 200) {
     fs.writeFile(filePath, body, { encoding: 'utf-8' }, (err) => {
-      console.log(err);
+      if (err) console.log(err);
     });
   }
 });
